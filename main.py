a550058f63a3e0810d87001b11036642e695e7fb
ahmetbulut@gmail.com <ahmetbulut@gmail.com>
2021-03-23 14:13:33 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+def f(n):
+    x = (2 + n) / 5
+    return x
+
+def g():
+    print('Hello, World')
+    return None
+
+result = f(50)
+
+if result > 100:
+    print(result)
+    print("Great Achievement!")
+elif result > 10:
+    print("Nice try, buddy!")
+else:
+    print(result)
+    print("Poor Result :-(")
